60857b5ab1c59162d0b0608b486c008a80c5984b
unknown <marcotulioescobedoamaro@gmail.com>
2020-02-17 14:27:32 -0600

    mi primer commit

---
+++ NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paragit;
+
+/**
+ *
+ * @author Marco
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+    int i=1;
+    System.out.print(i);    
+    }        
+            
+            
+}
