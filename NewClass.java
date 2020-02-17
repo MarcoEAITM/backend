2239976520f750503621b4bc2a6cb580defda37f
unknown <marcotulioescobedoamaro@gmail.com>
2020-02-17 14:37:35 -0600

    ahora el programa tiene un mensaje extra

--- NewClass.java
+++ NewClass.java
@@ -13,7 +13,7 @@ public class NewClass {
     
     public static void main(String[] args) {
     int i=1;
-    System.out.print(i);    
+    System.out.print(i+"asdasfdaas");    
     }        
             
             
